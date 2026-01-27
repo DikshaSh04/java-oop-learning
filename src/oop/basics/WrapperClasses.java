47824614ee390e47e08926f851202396f709e857
Diksha <diksha250406@gmail.com>
2026-01-27 14:42:55 +0530

    optimised code in WrapperClasses.java

--- src/oop/basics/WrapperClasses.java
+++ src/oop/basics/WrapperClasses.java
@@ -4,14 +4,20 @@ public class WrapperClasses {
     public static void main(String[] args) {
         //wrapper classes allow representation of primitives as objects
 
-        int a=10; //a is primitive datatype
+        int a=10;
         Integer b=a; //autoboxing: conversion of primitive to wrapper
 
         System.out.println("Primitive is:"+a);
-        System.out.println("Object is:"+b);
+        System.out.println("Object is:"+b.intValue());
 
-        Character ch='g'; //ch is object of type Character
+        Character ch='g';
         char c=ch; //unboxing: conversion of wrapper to primitive
+
+        System.out.println("Primitive is:"+c);
+        System.out.println("Object is:"+ch.charValue());
+
+//        intValue() and charValue() are wrapper class methods used to
+//        explicitly extract the corresponding primitive value from an object
 
 
     }
