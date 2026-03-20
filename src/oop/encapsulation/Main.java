5453efcaa55ab65ad4239ee171112872df1b83db
Diksha <diksha250406@gmail.com>
2026-03-21 00:01:08 +0530

    added code for encapsulation

---
+++ src/oop/encapsulation/Main.java
@@ -0,0 +1,15 @@
+package oop.encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Patient p1=new Patient("Brock",25,"B+");
+
+        p1.setAge(200);
+        p1.setAge(30);
+        p1.setDiagnosis("Viral Fever");
+        p1.addPrescription();
+
+        System.out.println("--------Patient Record--------");
+        p1.displayInfo();
+    }
+}
---
+++ src/oop/encapsulation/Patient.java
@@ -0,0 +1,72 @@
+package oop.encapsulation;
+
+public class Patient {
+    //fields
+    private String name;
+    private int age;
+    private String bloodGroup;
+    private String diagnosis;
+    private int prescriptionCount;
+
+    //constructor
+    Patient(String name,int age,String bloodGroup){
+        this.name=name;
+        setAge(age);
+        this.bloodGroup=bloodGroup;
+    }
+
+    //getters
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getBloodGroup(){
+        return bloodGroup;
+    }
+
+    public String getDiagnosis(){
+        return diagnosis;
+    }
+
+    public int getPrescriptionCount(){
+        return prescriptionCount;
+    }
+
+    //setters
+    public void setAge(int p_age){
+        if(p_age>=0 && p_age<=120){
+            this.age=p_age;
+            System.out.println("Age updated to "+p_age);
+        }
+        else{
+            System.out.println("Error! Age must be between 0 and 120.");
+        }
+
+    }
+
+    public void setDiagnosis(String diagnose){
+        this.diagnosis=diagnose;
+        System.out.println("Diagnosis updated to "+diagnose);
+    }
+
+    //method for incrementing prescription count
+    public void addPrescription(){
+        prescriptionCount+=1;
+        System.out.println("Prescription #"+this.getPrescriptionCount()+" added for "+this.getName());
+    }
+
+    //display method
+    public void displayInfo(){
+        System.out.println("Name: "+this.getName()+"\nAge: "+this.getAge()+"\nBlood Group: "+this.getBloodGroup()+"\nDiagnosis: "+this.getDiagnosis()+"\nPrescriptions Issued: "+this.getPrescriptionCount());
+    }
+
+
+
+
+
+
+}
